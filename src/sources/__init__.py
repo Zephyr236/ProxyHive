b38fac3d7b906efc39e17ae0bbd63e85e6e55fef
Zephyr236 <yhovtu@foxmail.com>
2025-04-07 23:11:24 +0800

    Create __init__.py

---
+++ src/sources/__init__.py
@@ -0,0 +1,32 @@
+from . import (
+    proxylist_geonode_com,
+    roosterkid_openproxylist,
+    proxifly_free_proxy_list,
+    sockslist_us,
+    proxyscrape_com,
+    www_proxy_list_download,
+    spys_one,
+    free_proxy_list_net,
+    sslproxies_org,
+    www_freeproxy_world,
+    proxydb_net,
+    zdaye_com,
+    hide_mn
+)
+
+
+modules = [
+    proxylist_geonode_com,
+    roosterkid_openproxylist,
+    proxifly_free_proxy_list,
+    sockslist_us,
+    proxyscrape_com,
+    www_proxy_list_download,
+    spys_one,
+    free_proxy_list_net,
+    sslproxies_org,
+    www_freeproxy_world,
+    proxydb_net,
+    zdaye_com,
+    hide_mn
+]
